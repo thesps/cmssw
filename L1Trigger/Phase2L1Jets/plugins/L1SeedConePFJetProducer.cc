a0493177e7bc74b7267d1cd189bae7c74a410914
Sioni Summers <ss5310@ic.ac.uk>
2020-07-07 15:35:35 +0200

    Add links to jet constituents

--- L1Trigger/Phase2L1Jets/plugins/L1SeedConePFJetProducer.cc
+++ L1Trigger/Phase2L1Jets/plugins/L1SeedConePFJetProducer.cc
@@ -1,11 +1,11 @@
 #include "L1Trigger/Phase2L1Jets/interface/L1SeedConePFJetProducer.hh"
 
-l1t::PFJet makeJet(std::vector<l1t::PFCandidate> parts){
+l1t::PFJet makeJet(std::vector< edm::Ptr< l1t::PFCandidate >> parts){
 
-    l1t::PFCandidate seed = parts.at(0);
+    l1t::PFCandidate seed = *parts.at(0);
 
-    auto sumpt = [](float a, const l1t::PFCandidate& b){
-        return a + b.pt();
+    auto sumpt = [](float a, const edm::Ptr<l1t::PFCandidate>& b){
+        return a + b->pt();
     };
 
     // Sum the pt
@@ -14,20 +14,23 @@ l1t::PFJet makeJet(std::vector<l1t::PFCandidate> parts){
     // pt weighted d eta
     std::vector<float> pt_deta;
     pt_deta.resize(parts.size());
-    std::transform(parts.begin(), parts.end(), pt_deta.begin(), [&seed, &pt](const l1t::PFCandidate &part){
-            return (part.pt() / pt) * (part.eta() - seed.eta());});
+    std::transform(parts.begin(), parts.end(), pt_deta.begin(), [&seed, &pt](const edm::Ptr<l1t::PFCandidate> &part){
+            return (part->pt() / pt) * (part->eta() - seed.eta());});
     // Accumulate the pt weighted etas. Init to the seed eta, start accumulating at begin()+1 to skip seed
     float eta = std::accumulate(pt_deta.begin()+1, pt_deta.end(), seed.eta());
 
     // pt weighted d phi
     std::vector<float> pt_dphi;
     pt_dphi.resize(parts.size());
-    std::transform(parts.begin(), parts.end(), pt_dphi.begin(), [&seed, &pt](const l1t::PFCandidate &part){
-            return (part.pt() / pt) * (part.phi() - seed.phi());});
+    std::transform(parts.begin(), parts.end(), pt_dphi.begin(), [&seed, &pt](const edm::Ptr<l1t::PFCandidate> &part){
+            return (part->pt() / pt) * (part->phi() - seed.phi());});
     // Accumulate the pt weighted phis. Init to the seed phi, start accumulating at begin()+1 to skip seed
     float phi = std::accumulate(pt_dphi.begin()+1, pt_dphi.end(), seed.phi());
 
     l1t::PFJet jet(pt, eta, phi);
+    for(auto it = parts.begin(); it != parts.end(); it++){
+        jet.addConstituent(*it);
+    }
 
     return jet;
 }
@@ -46,26 +49,28 @@ void L1SeedConePFJetProducer::produce(edm::Event& iEvent, const edm::EventSetup&
 
     edm::Handle<  l1t::PFCandidateCollection > l1PFCandidates;
     iEvent.getByToken( _l1PFToken, l1PFCandidates);
-    l1t::PFCandidateCollection work;
 
-    std::copy((*l1PFCandidates).begin(), (*l1PFCandidates).end(), std::back_inserter(work));
+    std::vector< edm::Ptr< l1t::PFCandidate >> work;
+    for(unsigned i = 0; i < (*l1PFCandidates).size(); i++){
+        work.push_back(edm::Ptr<l1t::PFCandidate>(l1PFCandidates, i));
+    }
 
-    std::sort(work.begin(), work.end(), [](l1t::PFCandidate i,l1t::PFCandidate j){return(i.pt() > j.pt());});   
+    std::sort(work.begin(), work.end(), [](edm::Ptr<l1t::PFCandidate> i,edm::Ptr<l1t::PFCandidate> j){return(i->pt() > j->pt());});   
 
     std::vector<l1t::PFJet> jets;
     jets.reserve(_nJets);
 
     while(!work.empty() && jets.size() < _nJets){
         // Take the first (highest pt) candidate as a seed
-        l1t::PFCandidate seed = work.at(0);
+        edm::Ptr<l1t::PFCandidate> seed = work.at(0);
         // Get the particles within a _coneSize of the seed
-        std::vector<l1t::PFCandidate> particlesInCone;
-        std::copy_if(work.begin(), work.end(), std::back_inserter(particlesInCone), [&](const l1t::PFCandidate &part){
-                return reco::deltaR<l1t::PFCandidate,l1t::PFCandidate>(seed, part) <= _coneSize;});
+        std::vector< edm::Ptr< l1t::PFCandidate >> particlesInCone;
+        std::copy_if(work.begin(), work.end(), std::back_inserter(particlesInCone), [&](const edm::Ptr<l1t::PFCandidate> &part){
+                return reco::deltaR<l1t::PFCandidate,l1t::PFCandidate>(*seed, *part) <= _coneSize;});
         jets.push_back(makeJet(particlesInCone));
         // remove the clustered particles
-        work.erase(std::remove_if(work.begin(), work.end(), [&](const l1t::PFCandidate &part){
-                return reco::deltaR<l1t::PFCandidate,l1t::PFCandidate>(seed, part) <= _coneSize;}), work.end());
+        work.erase(std::remove_if(work.begin(), work.end(), [&](const edm::Ptr<l1t::PFCandidate> &part){
+                return reco::deltaR<l1t::PFCandidate,l1t::PFCandidate>(*seed, *part) <= _coneSize;}), work.end());
     } 
     std::sort(jets.begin(), jets.end(), [](l1t::PFJet i,l1t::PFJet j){return(i.pt() > j.pt());}); 
     newPFJetCollection->swap(jets);
