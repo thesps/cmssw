c701231c7e569a44c2fe69907c49483d8d315387
Sioni Summers <14807534+thesps@users.noreply.github.com>
2026-02-09 11:53:27 +0100

    fixed after rebase

--- L1Trigger/Phase2L1ParticleFlow/test/make_l1ct_binaryFiles_cfg.py
+++ L1Trigger/Phase2L1ParticleFlow/test/make_l1ct_binaryFiles_cfg.py
@@ -89,6 +89,8 @@ l1ctLayer2SCJetsProducts = cms.VPSet([cms.PSet(jets = cms.InputTag("l1tSC4PFL1Pu
                                                nJets = cms.uint32(12),
                                                jetEncoding = cms.string("GTWide"))
                                       ])
+process.l1tLayer2SeedConeJetWriter = l1tSeededConeJetFileWriter.clone(outputFilename = cms.string('L1CTSCJetsPatterns'), collections = l1ctLayer2SCJetsProducts)
+
 l1ctLayer2SCJetsTM18Products = cms.VPSet([cms.PSet(jets = cms.InputTag("l1tSC4PFL1PuppiCorrectedTM18Emulator"),
                                                   nJets = cms.uint32(12),
                                                   mht  = cms.InputTag("l1tSC4PFL1PuppiCorrectedTM18EmulatorMHT"),
@@ -98,10 +100,8 @@ l1ctLayer2SCJetsTM18Products = cms.VPSet([cms.PSet(jets = cms.InputTag("l1tSC4PF
                                                   nJets = cms.uint32(12),
                                                   jetEncoding = cms.string("GTWide"))
                                          ])
-process.l1tLayer2SeedConeJetWriter = l1tSeededConeJetFileWriter.clone(outputFilename = cms.string('L1CTSCJetsPatterns'), collections = l1ctLayer2SCJetsProducts)
 process.l1tLayer2SeedConeJetTM18Writer = l1tSeededConeJetFileWriter.clone(outputFilename = cms.string('L1CTSCJetsTM18Patterns'), collections = l1ctLayer2SCJetsTM18Products)
-process.l1tLayer2NextGenJetWriter = l1tSeededConeJetFileWriter.clone(outputFilename = cms.string('L1CTNGJetsPatterns'), collections = l1ctLayer2NGJetsProducts)
-
+                                                                   
 l1ctLayer2SC4NGJetsProducts = cms.VPSet([cms.PSet(jets = cms.InputTag("l1tSC4NGJetProducer","l1tSC4NGJets"),
                                                nJets = cms.uint32(12),
                                                mht = cms.InputTag("l1tNGMHTPFProducer"),
